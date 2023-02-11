dcc33f80e48a703c62ea50815b7973b7c828a805
zainul-bashar <zainulbasharnazmin@gmail.com>
2023-02-11 23:47:05 +0530

    commiting

--- script.js
+++ script.js
@@ -7,21 +7,50 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
+  arr.map(function(el){
+      if(el.marks>50){
+        console.log(el);
+      }
+  })
+
   //Write your code here , just console.log
 }
 
 function PrintStudentsbyForEach() {
+  arr.forEach(function(el){
+    if(el.marks>50){
+      console.log(el);
+    }
+  })
   //Write your code here , just console.log
 }
 
 function addData() {
+  
+  arr.push({id:4,name:"Susain",age:20,marks:45});
+  console.log(arr);
+  
+
+
   //Write your code here, just console.log
 }
 
 function removeFailedStudent() {
+ const newArr= arr.filter((student)=>student.marks>=50)
+  console.log(newArr);
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
+  var thirdArr=[
+    {id:5,name:"Somu",age:19,marks:55},
+    {id:6,name:"Saima",age:21,marks:65},
+    {id:7,name:"Shuggu",age:19,marks:85},
+
+  ];
+ var result= arr.concat(thirdArr);
+ console.log(result);
+  
+
   //Write your code here, just console.log
 }
